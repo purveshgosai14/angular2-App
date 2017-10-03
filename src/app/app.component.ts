72f5f544d671c7b49f21244fecc3135ec0d9f236
Purvesh Gosai <puri.ui369@gmail.com>
2017-10-03 02:13:48 -0500

    Refactor appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,17 +23,19 @@ const HEROES: Hero[] = [
   template: `<h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
-  <h2>{{hero.name}} details!</h2>
+  <div *ngIf="selectedHero">
+  <h2>{{selectedHero.name}} details!</h2>
   <div>
-    <label>id: </label>{{hero.id}}
+    <label>id: </label>{{selectedHero.id}}
   </div>
   <div>
     <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name"/>
+    <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+  </div>
   </div>`,
   styles: [`
   .selected {
@@ -86,15 +88,16 @@ const HEROES: Hero[] = [
 `]
 })
 
-
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes= HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
+
 
 
 
